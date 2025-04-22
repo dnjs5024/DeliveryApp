477f0fdcf4ed647a169c003e241dda7256341cc4
YounGyoungMo <gyoungmo2011@naver.com>
2025-04-22 22:57:56 +0900

    Feat : #7 User 엔티티 및 BaseEntity 구현

---
+++ src/main/java/com/example/delivery/user/repository/UserRepository.java
@@ -0,0 +1,4 @@
+package com.example.delivery.user.repository;
+
+public interface UserRepository {
+}
---
+++ src/main/java/com/example/delivery/user/service/UserService.java
@@ -0,0 +1,8 @@
+package com.example.delivery.user.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface UserService {
+
+}
