e3bf2ae37370e1cfb2c2ff184a001ec65b60f61e
Prototype-Km <kgm970910@gmail.com>
2025-04-23 15:15:02 +0900

    chore : 주문 패키지 설계

---
+++ src/main/java/com/example/delivery/order/repository/OrderMenuRepository.java
@@ -0,0 +1,9 @@
+package com.example.delivery.order.repository;
+
+import com.example.delivery.order.entity.OrderMenu;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderMenuRepository extends JpaRepository<OrderMenu, Long> {
+}
