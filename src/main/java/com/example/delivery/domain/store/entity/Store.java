e9f7255e483dc6aaa917d04cbc25d5b16baf8975
mini-project-todo-list <leehanna0720@gmail.com>
2025-04-23 13:11:24 +0900

    :sparkles: Feat: store 엔티티 작성
    BREAKING CHANGE:

---
+++ src/main/java/com/example/delivery/domain/store/entity/Store.java
@@ -0,0 +1,67 @@
+package com.example.delivery.domain.store.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+import java.time.LocalTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Store {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+    private String description; // 가게 설명
+
+    @Column(nullable = false)
+    private LocalTime openTime;
+
+    @Column(nullable = false)
+    private LocalTime closeTime;
+
+    @Column(nullable = false)
+    private int minOrderPrice;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private StoreStatus status; // OPEN, CLOSED
+
+    //@ManyToOne(fetch = FetchType.LAZY)
+    //@JoinColumn(name="user_id), nullable = false)
+    //private User owner; // 사장님(User)
+
+    // 생성자, 비즈니스 메서드 등 추가 예정
+    // 생성 메서드
+//    @Builder
+//    public Store(String name, LocalTime openTime, LocalTime closeTime, Integer minOrderPrice, User owner) {
+//        this.name = name;
+//        this.openTime = openTime;
+//        this.closeTime = closeTime;
+//        this.minOrderPrice = minOrderPrice;
+//        this.status = StoreStatus.OPEN;
+//        this.owner = owner;
+//        this.createdAt = LocalDateTime.now();
+//        this.updatedAt = LocalDateTime.now();
+//    }
+
+    public void update(String name, LocalTime openTime, LocalTime closeTime, int minOrderPrice) {
+        this.name = name;
+        this.openTime = openTime;
+        this.closeTime = closeTime;
+        this.minOrderPrice = minOrderPrice;
+
+    }
+
+    public void changeStatus(StoreStatus newStatus) {
+        this.status = newStatus;
+    }
+
+}
