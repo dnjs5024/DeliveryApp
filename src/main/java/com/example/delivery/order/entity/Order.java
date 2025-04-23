67891dec62d00fc187767938be0b11cd810adb67
Prototype-Km <kgm970910@gmail.com>
2025-04-23 15:15:43 +0900

    feat(Order) : Order,OrderMenu 엔티티 완

---
+++ src/main/java/com/example/delivery/order/entity/Order.java
@@ -0,0 +1,46 @@
+package com.example.delivery.order.entity;
+
+import com.example.delivery.common.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Builder
+@Entity
+@Table(name ="order")
+public class Order extends BaseTimeEntity {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private OrderStatus orderStatus;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "user_id")
+//    private User user;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "sotre_id")
+//    private Store store;
+
+    //읽기전용 OrderMenu가 주인
+    @OneToMany(mappedBy = "order",cascade = CascadeType.REMOVE,orphanRemoval = true)
+    private List<OrderMenu> orderMenuList;
+
+    public Order(){;}
+
+    public Order(Long id, OrderStatus orderStatus, List<OrderMenu> orderMenuList) {
+        this.id = id;
+        this.orderStatus = orderStatus;
+        this.orderMenuList = orderMenuList;
+    }
+
+    //수정 메소드
+    public void changeStatus(OrderStatus orderStatus){
+        this.orderStatus = orderStatus;
+    }
+}
---
+++ src/main/java/com/example/delivery/order/entity/OrderMenu.java
@@ -0,0 +1,33 @@
+package com.example.delivery.order.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.apache.catalina.Store;
+
+@Builder
+@Entity
+@Table(name ="order_menu")
+public class OrderMenu {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int quantity; //수량
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name ="order_id")
+    private Order order;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name ="menu_id")
+//    private Menu menu;
+
+    public OrderMenu(){;}
+
+    public OrderMenu(Long id, int quantity, Order order) {
+        this.id = id;
+        this.quantity = quantity;
+        this.order = order;
+    }
+}
