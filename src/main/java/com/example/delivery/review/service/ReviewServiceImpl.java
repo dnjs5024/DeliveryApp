2649b11379d71321b85b56ed6a3bab5d5bbb4a4b
dnjs5024 <66933008+dnjs5024@users.noreply.github.com>
2025-04-24 10:17:29 +0900

    Update ReviewServiceImpl.java

--- src/main/java/com/example/delivery/review/service/ReviewServiceImpl.java
+++ src/main/java/com/example/delivery/review/service/ReviewServiceImpl.java
@@ -28,7 +28,7 @@ public class ReviewServiceImpl implements ReviewService{
     @Override
     public ReviewSaveResponseDto save(ReviewSaveRequestDto requestDto, List<MultipartFile> files) {
 
-        if(!files.isEmpty()){
+        if(files != null && !files.isEmpty()){
             List<String> urlList = imageUploadService.uploadFile(files);
             urlList.forEach(System.out::println);
         }
