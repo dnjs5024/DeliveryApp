23575fcbfe4e604536448ce5d5b8d82c401d930e
dnjs5024 <dnjs7469@naver.com>
2025-04-25 10:56:43 +0900

    "Fix : 병합 전 커밋"

--- src/main/java/com/example/delivery/domain/user/controller/UserController.java
+++ src/main/java/com/example/delivery/domain/user/controller/UserController.java
@@ -12,12 +12,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import com.example.delivery.domain.user.dto.UserRequestDto;
 import com.example.delivery.domain.user.dto.UserResponseDto;
-import com.example.delivery.domain.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,7 +34,7 @@ public class UserController {
     @PostMapping("/signup")
     public ResponseEntity<ApiResponseDto<UserResponseDto>> signup(@Valid @RequestBody UserRequestDto dto)
     {
-        UserResponseDto userResponseDto = userService.signup(dto.getEmail(), dto.getPassword(), dto.getRole(), dto.getUsername());
+        UserResponseDto userResponseDto = userService.signup(dto.email(), dto.password(), dto.role(), dto.username());
         return ResponseEntity.status(
                 SuccessCode.SIGNUP_SUCCESS.getHttpStatus()).body(
                 ApiResponseDto.success(SuccessCode.SIGNUP_SUCCESS, userResponseDto));
