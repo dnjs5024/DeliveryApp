9465bbfb85e0e6118eee1fdd7d74446fdba902f1
Prototype-Km <kgm970910@gmail.com>
2025-04-25 16:23:02 +0900

    feat (Order) : 주문 페이징/ 상세보기

--- src/main/java/com/example/delivery/domain/order/controller/OrderOwnerController.java
+++ src/main/java/com/example/delivery/domain/order/controller/OrderOwnerController.java
@@ -1,17 +1,50 @@
 package com.example.delivery.domain.order.controller;
 
 
+import com.example.delivery.common.exception.enums.SuccessCode;
+import com.example.delivery.common.response.ApiResponseDto;
+import com.example.delivery.domain.order.dto.responseDTO.OrderResponseDTO;
+import com.example.delivery.domain.order.service.OrderService;
+import com.example.delivery.domain.user.dto.SessionUserDto;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/owner")
 public class OrderOwnerController {
 
-    //주문 목록 (owner) get -> 페이징 Slice로
-    //주문 상세조회 get
+    private final OrderService orderService;
+
+    //주문 목록 (owner)-> 페이징 Slice,hasNext로 꺼내기
+    @GetMapping("/store/{storeId}/orders")
+    public ResponseEntity<ApiResponseDto<Slice<OrderResponseDTO>>> getList(
+            @SessionAttribute(name = "loginUser") SessionUserDto sessionUserDto,
+            @PathVariable Long storeId,
+            Pageable pageable)
+    {
+        Long userId = sessionUserDto.getId();
+        Slice<OrderResponseDTO> slice =
+                orderService.getStoreOrders(userId,storeId,pageable);
+
+        return ResponseEntity.status(SuccessCode.ORDER_PAGING_SUCCESS.getHttpStatus())
+                .body(ApiResponseDto.success(SuccessCode.ORDER_PAGING_SUCCESS,slice));
+    }
+
+    //주문 상세조회  -> 유저정보를 더 추가하면 dto새로파서
+    @GetMapping("/store/{storeId}/orders/{orderId}")
+    public ResponseEntity<ApiResponseDto<OrderResponseDTO>> getOrder(@SessionAttribute(name="loginUser") SessionUserDto sessionUserDto,@PathVariable Long storeId,@PathVariable Long orderId){
+        Long userId = sessionUserDto.getId();
+
+        OrderResponseDTO detail = orderService.getDetail(userId, storeId, orderId);
+        return ResponseEntity.status(SuccessCode.OK.getHttpStatus())
+                .body(ApiResponseDto.success(SuccessCode.OK,detail));
+    }
     //주문 수락 patch
     //주문 거절 patch
     //음식 주문완료 patch
