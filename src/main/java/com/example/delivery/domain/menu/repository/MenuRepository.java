34ce827f19e5cdb9d17cc835f13457e959b35d04
YounGyoungMo <gyoungmo2011@naver.com>
2025-04-26 19:25:50 +0900

    fix: 병합 충돌 해결2

--- src/main/java/com/example/delivery/domain/menu/repository/MenuRepository.java
+++ src/main/java/com/example/delivery/domain/menu/repository/MenuRepository.java
@@ -1,6 +1,5 @@
 package com.example.delivery.domain.menu.repository;
 
-import com.example.delivery.domain.store.entity.Store;
 import com.example.delivery.domain.menu.entity.Menu;
 import com.example.delivery.common.exception.base.NotFoundException;
 import com.example.delivery.common.exception.enums.ErrorCode;
@@ -24,5 +23,6 @@ public interface MenuRepository extends JpaRepository <Menu,Long> {
     //List<Menu> findAllByStoreAndDeletedFalse(Store store); <이것도 오류가 나서 JPQL로 작성
     @Query("SELECT m FROM Menu m WHERE m.store = :store AND m.deleted = false")
     List<Menu> findActiveMenusByStore(@Param("store") Store store);
+
 
 }
